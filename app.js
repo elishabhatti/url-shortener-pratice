7081961dccb7f7bc893e731879106dab97b7085f
elishabhatti <elishakumari2007@gmail.com>
2025-04-04 18:07:43 +0500

    added template

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("HI");
+  res.render("index");
 });
 
 app.listen(3000);
