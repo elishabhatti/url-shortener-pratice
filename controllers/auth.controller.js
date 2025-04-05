55423ce8fa9062e8cf438bde01c1dcc8b060bc51
elishabhatti <elishakumari2007@gmail.com>
2025-04-05 17:09:50 +0500

    added hashed password with argon 2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,15 @@
-import { createUser } from "../services/auth.services.js";
+import { createUser, hashedPassword } from "../services/auth.services.js";
 
 export const getRegisterPage = async (req, res) => {
   res.render("auth/register");
 };
 
 export const postRegister = async (req, res) => {
-    let {name, email, password} = req.body
-    console.log(name, email, password);
-    let user = await createUser({name, email, password})
-    console.log(user);
-    
-    
-    res.redirect("/")
-    
-}
+  let { name, email, password } = req.body;
+  console.log(name, email, password);
+  await hashedPassword(password)
+  let user = await createUser({ name, email, password });
+  console.log(user);
+
+  res.redirect("/");
+};
--- services/auth.services.js
+++ services/auth.services.js
@@ -1,5 +1,6 @@
 import { db } from "../config/db.config.js";
 import { usersTable } from "../drizzle/schema.js";
+import argon2 from "argon2";
 
 export const createUser = async ({ name, email, password }) => {
   const [user] = await db
@@ -7,4 +8,8 @@ export const createUser = async ({ name, email, password }) => {
     .values({ name, email, password })
     .$returningId();
   return user;
+};
+
+export const hashedPassword = async (password) => {
+  return await argon2.hash(password, 12);
 };
