3faa6eb2b3338bcf7e14c1167c63ade538abae1d
elishabhatti <elishakumari2007@gmail.com>
2025-04-05 15:20:37 +0500

    added user table schema updated

--- drizzle/0000_dapper_killmonger.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `shortlink` (
-	`id` serial AUTO_INCREMENT NOT NULL,
-	`url` varchar(2000) NOT NULL,
-	`shortCode` varchar(255) NOT NULL,
-	CONSTRAINT `shortlink_id` PRIMARY KEY(`id`),
-	CONSTRAINT `shortlink_shortCode_unique` UNIQUE(`shortCode`)
-);
--- drizzle/schema.js
+++ drizzle/schema.js
@@ -1,34 +1,39 @@
 import { relations } from "drizzle-orm";
-import { timestamp } from "drizzle-orm/mysql-core";
-import { int, mysqlTable, serial, varchar } from "drizzle-orm/mysql-core";
+import {
+  boolean,
+  int,
+  mysqlTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/mysql-core";
 
-export const userTable = mysqlTable("users", {
-  id: serial().primaryKey(),
-  name: varchar({ length: 20 }).notNull(),
+export const shortLink = mysqlTable("short_link", {
+  id: int().autoincrement().primaryKey(),
+  url: varchar({ length: 255 }).notNull(),
+  shortCode: varchar("short_code", { length: 20 }).notNull().unique(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
+  userId: int("user_id")
+    .notNull()
+    .references(() => usersTable.id),
+});
+
+export const usersTable = mysqlTable("users", {
+  id: int().autoincrement().primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   password: varchar({ length: 255 }).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
 });
 
-export const shortLink = mysqlTable("shortlink", {
-  id: serial().primaryKey(),
-  url: varchar({ length: 2000 }).notNull(),
-  shortCode: varchar({ length: 255 }).notNull().unique(),
-  userId: int("userid")
-    .notNull()
-    .references(() => userTable.id),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
-});
-
-export const userRelation = relations(userTable, ({ many }) => {
-  shortLink: many(shortLink);
-});
-
-export const shortLinkRelation = relations(shortLink, ({ one }) => {
-  user: one(userTable, {
-    fields: [shortLink.userId],
-    references: [userTable.id],
-  });
-});
+export const usersRelation = relations(usersTable, ({ many }) => ({
+  shortLink: many(shortLink),
+}));
+// A short link belongs to a user
+export const shortLinksRelation = relations(shortLink, ({ one }) => ({
+  user: one(usersTable, {
+    fields: [shortLink.userId], //foreign key
+    references: [usersTable.id],
+  }),
+}));
