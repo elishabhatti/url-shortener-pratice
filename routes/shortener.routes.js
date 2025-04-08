1ee53f666c91dc690e9b211b9d5f81ab6dc9fe93
elishabhatti <elishakumari2007@gmail.com>
2025-04-08 12:35:08 +0500

    changed the route of the deleteshort code

--- routes/shortener.routes.js
+++ routes/shortener.routes.js
@@ -13,7 +13,7 @@ export const shortenerRouter = Router();
 shortenerRouter.get("/", getShortenerPage);
 shortenerRouter.post("/addShortCode", postShortCode);
 shortenerRouter.get("/urls/:shortCode", redirectToShortLink);
-shortenerRouter.get("/deleteshortCode/:id", deleteShortCode);
+shortenerRouter.get("/deleteShortCode/:id", deleteShortCode);
 shortenerRouter.get(
   "/getUpdateShortCodePageById/:id",
   getUpdateShortCodePageById
