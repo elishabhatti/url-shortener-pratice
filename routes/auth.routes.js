92598ad9f358ce0d1ede9edf3dafcea4023afb36
elisha <elishajameel2007@gmail.com>
2025-04-14 23:24:17 -0700

    chnaged the route of verify email link

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -16,9 +16,9 @@ export const authRouter = Router();
 
 authRouter.route("/register").get(getRegisterPage).post(postRegister);
 authRouter.route("/login").get(getLoginPage).post(postLogin);
-authRouter.route("/profile").get(getProfilePage)
-authRouter.route("/verify-email").get(getVerifyEmailPage)
-authRouter.route("/resend-verification-link").post(resendVerificationLink)
-authRouter.route("/resend-email-token").get(verifyEmailToken)
+authRouter.route("/profile").get(getProfilePage);
+authRouter.route("/verify-email").get(getVerifyEmailPage);
+authRouter.route("/resend-verification-link").post(resendVerificationLink);
+authRouter.route("/verify-email-token").get(verifyEmailToken);
 authRouter.route("/me").get(getMePage);
 authRouter.route("/logout").get(logoutUser);
